296d745d4c1fa5bc338407d17c1e95aa1c5c4f36
Andrew <56526926+frontandrew@users.noreply.github.com>
2025-01-21 12:20:02 +0500

    [sprint5.1] ui kit

--- src/shared/components/layouts/two-colimn-layout/view.tsx
+++ src/shared/components/layouts/two-colimn-layout/view.tsx
@@ -3,12 +3,8 @@ import { FC, ReactNode } from 'react'
 import style from './style.module.css'
 
 export const TwoColumnLayout: FC<{ left?: ReactNode, right?: ReactNode }> = ({ left, right }) => (
-  <div className={style.layout}>
-    <section className={style.content}>
-      {left}
-    </section>
-    <section className={style.content}>
-      {right}
-    </section>
+  <div className={style.layout} id={'two-column-layout'}>
+    <section className={style.content} id={'two-column-layout--left'}>{left}</section>
+    <section className={style.content} id={'two-column-layout--right'}>{right}</section>
   </div>
 )
