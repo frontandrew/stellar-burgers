ccf788d1105bd9c936fc23fab6a1077a2373a0cc
AndrewBubnov <jackblack.nt@gmail.com>
2025-01-24 00:41:29 +0500

    chore: configure cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3002/",
+    viewportWidth: 1280,
+    viewportHeight: 720,
   },
 });
