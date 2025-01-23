66ea2de09f7397444eb405a2651ad7e8d7dbf0b6
AndrewBubnov <jackblack.nt@gmail.com>
2025-01-24 00:50:50 +0500

    feat: authentication test

---
+++ cypress/constants/index.ts
@@ -0,0 +1,7 @@
+export const testParams = {
+  accessToken: 'test-access-token',
+  refreshToken: 'test-refresh-token',
+  email: 'test@mail.box',
+  name: 'Test User',
+  password: 'test-password',
+} as const;
---
+++ cypress/e2e/authentication.cy.ts
@@ -0,0 +1,26 @@
+import type {} from 'cypress'
+
+import { testParams } from '../constants'
+
+describe('Application authentication', () => {
+  it('should provide access to the profile page', async () => {
+    const { email, password, name } = testParams
+
+    cy.intercept('POST', '**/login', { fixture: 'login'}).as('login')
+    cy.intercept('GET', '**/ingredients', { success: false, data: [] })
+    cy.intercept('GET', '**/user', { success: false })
+
+    cy.visit('/login')
+
+    cy.get('[data-test-id=email-input]').focus().type(email)
+    cy.get('[data-test-id=password-input]').focus().type(password)
+    cy.get('[data-test-id=login-submit]').click()
+
+    cy.wait('@login')
+
+    cy.get('[data-test-id=navigate-profile]').click()
+
+    cy.get('[data-test-id=email-input]').should('have.value', email)
+    cy.get('[data-test-id=name-input]').should('have.value', name)
+  })
+})
